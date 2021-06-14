528866448e9c88a7b2a1985753c9c5ac6bb0948f
alambrow <alexander.lambrow@gmail.com>
2021-06-14 16:32:21 -0500

    modified nav bar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,25 +1,29 @@
 import React from "react";
+import { NavDropdown, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
+import Navbar from 'react-bootstrap/Navbar'
 
 export const NavBar = (props) => {
     return (
-        <ul className="navbar">
-        <li className="navbar__item">
-            <Link className="navbar__link" to="/">Home</Link>
-        </li>
-        <li className="navbar__item">
-            <Link className="navbar__link" to="/locations">Favorites</Link>
-        </li>
-        <li className="navbar__item">
-            <Link className="navbar__link" to="/animals">Shared Venues</Link>
-        </li>
-        <li className="navbar__item">
-                <Link className="navbar__link" onClick={() => {
-                    localStorage.removeItem("vibehunt_memberId")
-                }
-                } to="/login">Logout</Link>
-            </li>
-        </ul>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Collapse>
+                <Nav className="vibehunt__nav">
+                <NavDropdown title="VibeHunt" id="basic-nav-dropdown">
+                    <NavDropdown.Item>
+                        <Link className="navbar__link" to="/favorites">Favorites</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item>
+                        <Link className="navbar__link" to="/shared_venues">Shared Venues</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item>
+                        <Link className="navbar__link" onClick={() => {
+                            localStorage.removeItem("vibehunt_memberId")}
+                        } to="/login">Logout</Link>
+                    </NavDropdown.Item>
+                </ NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
